9fcf67386905f5fdb4165b0bfe27a7c02ab78727
RomyMartinez <ngcsmrtnz@gmail.com>
2023-07-22 23:12:27 -0300

    ex01

--- estruturas-repeticao-2/src/ex01.java
+++ estruturas-repeticao-2/src/ex01.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ex01 {
     public static void main(String[] args) throws Exception {
-
+        int Numeros, TotalPositivo = 0, negativo = 0;
+        @SuppressWarnings("resource")
+        Scanner in = new Scanner(System.in);
+        for (int i = 1; i < 20; i++) {
+            System.out.print("Digite um numero: ");
+            Numeros = in.nextInt();
+            if (Numeros > 0) {
+                TotalPositivo = TotalPositivo + Numeros;
+            } else {
+                negativo++;
+            }
+        }
+        System.out.print("\nSoma dos positivos: " + TotalPositivo);
+        System.out.println("Total de negativos: " + negativo);
     }
 }
